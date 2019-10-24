cb0414e25c05bbfadbcd4d47206a56b24c2c6211
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2019-10-24 11:34:48 -0500

    Add PurgeCSS

--- slate.config.js
+++ slate.config.js
@@ -4,6 +4,9 @@
 // For more information, visit https://github.com/Shopify/slate/wiki/Slate-Configuration
 
 const path = require('path');
+const PostcssCssnano = require("cssnano")()
+const PostcssAutoprefixer = require("autoprefixer")({ browsers: 'last 2 versions' })
+const PostcssPurgeCss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
@@ -15,4 +18,20 @@ module.exports = {
       },
     },
   },
+  'webpack.postcss.plugins': (config) => {
+    const plugins = [];
+    if (process.env.NODE_ENV === 'production') {
+      plugins.push(
+        PostcssPurgeCss({
+            content: ['./src/**/*.liquid'],
+            whitelistPatterns: [/^(is-|has-|will-|js-)/],
+            keyframes: true,
+            fontFace: true
+        }),
+        PostcssAutoprefixer,
+        PostcssCssnano
+      )
+    }
+    return plugins;
+  }  
 };
